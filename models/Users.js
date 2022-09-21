df66f3adba894911c4edb02f52bb436ddcc73c8e
Sujana Bhandari <sujana6589@gmail.com>
2022-09-21 15:05:00 +0200

    Add Auth

--- models/Users.js
+++ models/Users.js
@@ -4,32 +4,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // var validator = require('validator');
 
-const { SECRET_KEY } = process.env;
 
 const userSchema = new Schema({
   userName: { type: String, unique: true, minLength: 2, maxLength: 255 },
-  password: { type: String},
+  password: {  type: String, required: [true, 'Password is required']},
   email: { type: String, required: true },
-
   profilePic: { type: String},
+
   ratingIds: [{ type: Schema.Types.ObjectId, ref: "Rating" }],
   //Reference Should be offers or users?
-  role: {type:String, enum:['creator_id', 'customer', 'guest']},
+  // role: {type:String, enum:['creator_id', 'customer', 'guest']},
   _offers: [{ type: Schema.Types.ObjectId, ref: "Offer" }],
   _orders: [{ type: Schema.Types.ObjectId, ref: "Order" }],
   // _customerOrderIds: [{ type: Schema.Types.ObjectId, ref: "Order" }]
 });
 
-// userSchema.methods.generateToken = function () {
-//   const payload = {
-//     _id: this._id,
-//     user_name: this.user_name,
-//     email: this.email,
-//   };
-//   const token = jwt.sign(payload, SECRET_KEY);
-//   console.log(token);
-//   return token;
-// };
+userSchema.methods.generateToken = function () {
+  const payload = {
+    _id: this._id,
+    userName: this.userName,
+    email: this.email,
+    profilePic: this.profilePic,
+  };
+  const token = jwt.sign(payload, process.env.SECRET_KEY);
+  console.log(token);
+  return token;
+};
 
 const User = mongoose.model("User", userSchema);
 
