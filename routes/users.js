92e6fd9a13a92cfaa7ac7bb311c613a031ad4735
Sujana Bhandari <sujana6589@gmail.com>
2022-09-26 18:14:32 +0200

    Create route for orders

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,13 @@ const {
     authorizeAdmin
   } = require("../middlewares/authorizeAdmin");
 
-const {registerUser, loginUser, getUser, get_all_users, authenticate_self, retrieve_all_users_id} = require("../controllers/auth");
+const {registerUser, loginUser, getUser} = require("../controllers/auth");
 
 usersRouter.post('/signin', loginUser);
 usersRouter.get('/me', authorizeAdmin, getUser);
 
 usersRouter.route("/signup").post(upload.single('profilePic'),registerUser);
 
-usersRouter.get('/users', get_all_users);
-usersRouter.get('/users/:id', retrieve_all_users_id);
+// usersRouter.get('/users', get_all_users);
+// usersRouter.get('/users/:id', retrieve_all_users_id);
 module.exports = usersRouter;
