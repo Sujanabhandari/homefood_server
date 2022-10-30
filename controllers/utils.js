b56816288c9b534e0318b0164ea64fedffa07796
Sujana Bhandari <sujana6589@gmail.com>
2022-10-30 20:37:51 +0100

    Remove duplicate bucket name from s3 object url
    In local, s3 object url was correct as shown in s3 properties.
    However, in render server saved urls had extra bucket name in
    object url.
    
    This commit always saves correct url no matter the environments.

---
+++ controllers/utils.js
@@ -0,0 +1,14 @@
+const { AWS_BUCKET_NAME } = process.env;
+
+const getAmazonS3Url = (url, key) => {
+    let tmpUrl = url.split(AWS_BUCKET_NAME + '.');
+    if (tmpUrl.length == 2) {
+        const s3Url = tmpUrl[1].split('/' + key);
+        if (s3Url.length >= 1) {
+            return `https://${AWS_BUCKET_NAME}.${s3Url[0]}/${key}`;
+        }
+    }
+    return url;
+}
+
+module.exports = getAmazonS3Url;
